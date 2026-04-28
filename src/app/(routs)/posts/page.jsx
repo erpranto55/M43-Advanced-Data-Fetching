5b65f0100c05fc3c1408ef4d0df97f5984fa13f2
erpranto <erpranto55@gmail.com>
2026-04-28 10:53:46 +0600

    Fetching Data in server component

--- src/app/(routs)/posts/page.jsx
+++ src/app/(routs)/posts/page.jsx
@@ -1,9 +1,33 @@
+// const getPosts = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     return res.json();
+// }
+const getPosts = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    if (!res.ok) {
+        throw new Error('Failed to fetch posts.');
+    }
+    return res.json();
+}
 
+// const getPosts = async () => {
+//     try {
+//         const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//         return res.json();
+//     }
+//     catch (err) {
+//         throw new Error('Failed to fetch posts.')
+//     }
+// }
 
-const PostsPage = () => {
+const PostsPage = async () => {
+    // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // const posts = await res.json();
+
+    const posts = await getPosts();
     return (
         <div>
-            <h2>Post are coming soon.</h2>
+            <h2>Post are coming soon:   {posts.length}</h2>
         </div>
     );
 };
