1ae427e80c9aec5578d8e6327a98f3a002a2cc3c
erpranto <erpranto55@gmail.com>
2026-04-28 12:01:41 +0600

    Force Cache and Cache checking

---
+++ src/app/(routs)/products/page.jsx
@@ -0,0 +1,27 @@
+import Product from "@/components/Product";
+
+const getProducts = async () => {
+    const res = await fetch('http://localhost:5000/products', { cache: "force-cache" });
+    return res.json();
+}
+
+const ProductsPage = async () => {
+    const products = await getProducts();
+
+    return (
+        <div className="container mx-auto">
+            <h2>Products: {products.length}</h2>
+            <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {
+                    products.map(product =>
+                        <Product
+                            key={product.id}
+                            product={product}
+                        />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ProductsPage;
